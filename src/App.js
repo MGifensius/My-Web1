67a3b6de0909ebbf105d7ffcd9e5ec884245cad3
Marchelino Gifensius <100520193+MGifensius@users.noreply.github.com>
2022-02-28 19:24:24 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import FavouritePage from './pages/Favourites';
+
+function App() {
+  return (
+    <div>Hello World!
+      
+    </div>
+  );
+}
+
+export default App;
